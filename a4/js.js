9eea0bae35259be228bcae90168aae762901aad3
Michelle <mtang2@gmu.edu>
2021-11-28 15:51:55 -0500

    a4

---
+++ a4/js.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  $("#funny-btn").click(function(){
+    $("p").hide();
+  });
+
+  $(".radio-grp").click(function(){
+    var $element = $(this);
+    var $city = $("label[for='" + $element.attr('id') + "']").text();
+    $("#city-text").text($city);
+    $("#city-text").attr("hidden", false);
+  });
+
+  $( function() {
+    $( "#datepicker" ).datepicker();
+  } );
+
+  $( function() {
+    $( "#accordion" ).accordion();
+  } );
+
+  $( function() {
+    $( "#doge" ).resizable();
+  } );
+});
